b52c43922cc18043cc68fcc7f838cea2ee6f365e
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-03 16:43:19 +0200

    Content component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
   import headerComp from './components/headerComp.vue'
+  import contentComp from './components/contentComp.vue'
   export default{
     name: 'App',
     components: {
-      headerComp
+      headerComp,
+      contentComp
     }
   }
 </script>
 
 <template>
   <headerComp/>
+  <contentComp/>
 </template>
 
 <style>
---
+++ src/components/contentComp.vue
@@ -0,0 +1,25 @@
+<script>
+  export default{
+    name: 'contentComp',
+  }
+</script>
+
+<template>
+    <div>
+        <div class="container py-4">
+            <h2 class="text-white"> ... Content goes here ... </h2>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    div{
+        background-color: #1c1c1c;
+        div{
+            h2{
+                font-family: 'Asap Condensed', sans-serif;
+            }
+        }
+    }
+</style>
