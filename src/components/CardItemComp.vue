715844d678d33ad8330ccde4b6668359c80ba7b9
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-04 16:24:29 +0200

    Creazione componente "card" e inserimento props

---
+++ src/components/CardItemComp.vue
@@ -0,0 +1,24 @@
+<script>
+    export default{
+        name: 'CardItemComp',
+        props: ['ArrayCard']
+    }
+</script>
+
+<template>
+    <li class="my-4">
+        <img :src="ArrayCard.thumb" alt="">
+        <p class="mt-3 text-start">{{ ArrayCard.series }}</p>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+    li{
+        list-style: none;
+        width: calc((100% / 6) - 1rem);
+        img{
+            height: 10rem;
+            width: 100%;
+        }
+    }
+</style>
